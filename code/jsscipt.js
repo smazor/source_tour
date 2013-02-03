a7cf8ed2e6db7795b9f3c78808c520c8bcb7f440
smazor <smazor.shai@gmail.com>
2013-02-03 22:55:06 +0200

    split

--- code/jsscipt.js
+++ code/jsscipt.js
@@ -1,2 +1,15 @@
 document.write(str.substring(3)+"<br>");
 document.write(str.substring(3,7))
+
+
+// slice 
+// syntax: string.slice(start [, stop])
+"Good news, everyone!".slice(5,9); // extracts 'news'
+
+// substring 
+// syntax: string.substring(start [, stop])
+"Good news, everyone!".substring(5,9); // extracts 'news'
+
+// substr
+// syntax: string.substr(start [, length])
+"Good news, everyone!".substr(5,4); // extracts 'news'
